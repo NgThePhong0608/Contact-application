6c1a945b87afafede64f87827291a416f3acf996
PhongNT <phongnt@kaopiz.com>
2024-02-07 23:15:47 +0700

    Add SimpleSoftDeleteScope to Contact model and fix indentation in show.blade.php

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\SimpleSoftDeleteScope;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'first_name', 'last_name', 'phone', 'email', 'address', 'company_id'
@@ -22,5 +24,13 @@ class Contact extends Model
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new SimpleSoftDeleteScope);
+        // static::addGlobalScope('softDeletes', function (Builder $builder) {
+        //     $builder->whereNull('deleted_at');
+        // });
     }
 }
--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -50,17 +50,17 @@
                                     <div class="form-group row mb-0">
                                         <div class="col-md-9 offset-md-3">
                                             <a href=" {{ route('admin.contacts.edit', $contact->id) }}"
-                                                class="btn btn-info">Edit</a>
+                                               class="btn btn-info">Edit</a>
                                             <form
-                                                action="{{ route('contacts.destroy', ['contact' => $contact->id, 'redirect' => 'admin.contacts.index']) }}"
+                                                action="{{ route('contacts.destroy', ['contact' => $contact->id, 'redirect' => 'contacts.index']) }}"
                                                 method="POST" style="display: inline">
                                                 @csrf
                                                 @method('delete')
                                                 <button type="submit" class="btn btn-outline-danger"
-                                                    title="Delete">Delete</button>
+                                                        title="Delete">Delete</button>
                                             </form>
                                             <a href="{{ route('admin.contacts.index') }}"
-                                                class="btn btn-outline-secondary">Cancel</a>
+                                               class="btn btn-outline-secondary">Cancel</a>
                                         </div>
                                     </div>
                                 </div>
